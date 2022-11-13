3b73613eb2dc764e8f597d7c36cf5041ae957069
KBModib <kmodibedi@outlook.com>
2022-11-13 17:24:07 +0200

    grid

--- src/Components/Summer.js
+++ src/Components/Summer.js
@@ -5,6 +5,18 @@ import { onAuthStateChanged, } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 //import { CartProduct } from './CartProducts'
 import './CardStyle.css';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Summer = (props) => {
     function GetUserUid() {
@@ -76,6 +88,11 @@ const Summer = (props) => {
     };
     return (
         <>
+           
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
             {products.length > 0 && (
                 <div className='container-fluid'>
                     <div className='productBox' >
@@ -88,6 +105,10 @@ const Summer = (props) => {
             {products.length < 1 && (
                 <div className='container-fluid'>Please wait...</div>
             )}
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
         </>
     )
 }
--- src/Components/Winter.js
+++ src/Components/Winter.js
@@ -4,6 +4,18 @@ import { Products } from './Products'
 import { onAuthStateChanged, } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 //import { CartProduct } from './CartProducts'
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}))
 
 const Winter = (props) => {
     function GetUserUid() {
@@ -75,18 +87,26 @@ const Winter = (props) => {
     };
     return (
         <>
-            {products.length > 0 && (
-                <div className='container-fluid'>
-                    <div className='productBox' >
-                        <div>
-                            <Products products={products} helloWorld={helloWorld} />
+           <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {Array.from(Array(6)).map((_, index) => (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                    {products.length > 0 && (
+                        <div className='container-fluid'>
+                            <div className='productBox' >
+                                <div>
+                                    <Products products={products} helloWorld={helloWorld} />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            )}
-            {products.length < 1 && (
-                <div className='container-fluid'>Please wait...</div>
-            )}
+                    )}
+                    {products.length < 1 && (
+                        <div className='container-fluid'>Please wait...</div>
+                    )}
+                </Grid>
+                ))}
+            </Grid>
+            </Box>
         </>
     )
 }
