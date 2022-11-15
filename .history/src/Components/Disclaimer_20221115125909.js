5c94e6947f23420e4db961a357c028db263c3c24
Bongani <bonganicele806@gmail.com>
2022-11-15 14:09:57 +0200

    addedCss

---
+++ .history/src/Components/Disclaimer_20221115125909.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import fire, { auth } from '../Config/Config'
+import { onAuthStateChanged, } from 'firebase/auth'
+import { Navbar } from './Navbar'
+
+export default function Disclaimer() {
+
+  function GetUserUid() {
+    const [uid, setUid] = useState(null);
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                setUid(user.uid);
+            }
+        })
+    }, [])
+    return uid;
+}
+const uid = GetUserUid();
+function GetCurrentUser() {
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        const unbn = onAuthStateChanged(auth, user => {
+            if (user) {
+                fire.firestore().collection("user").doc(user.uid).get().then(snapshot => {
+                    setUser(snapshot.data().FullName)
+                })
+            } else {
+
+
+                setUser(null)
+            }
+        })
+        return unbn
+    }, [])
+    return user;
+
+
+}
+const user = GetCurrentUser()
+  return (
+    <>
+      <Navbar user={user} />
+      <div>
+      <div className="Staticpages">
+        <h1 className='Heading'>Disclaimer</h1>
+        <div className='Info-section'>
+          <h3 className='ques'>Reservation of Rights</h3>
+          <p className='ans'>We reserve the right at any time and in its sole discretion to request that you remove all links or any particular link to our Web site. You agree to immediately remove all links to our Web site upon such request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuing to link to our Web site, you agree to be bound to and abide by these linking terms and conditions.</p>
+          <h3 className='ques'>Content Liability</h3>
+          <p className='ans'>We shall have no responsibility or liability for any content appearing on your Web site. You agree to indemnify and defend us against all claims arising out of or based upon your Website. No link(s) may appear on any page on your Web site or within any context containing content or materials that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
+          <h3 className='ques'>License</h3>
+          <p className='ans'>Unless otherwise stated, Faith Revelations and/or it's licensors own the intellectual property rights for all material on Faith Revelations. All intellectual property rights are reserved. You may view and/or print pages from https://faithrevelations.co.za for your own personal use subject to restrictions set in these terms and conditions.</p>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ .history/src/Components/Slider_20221115113201.js
@@ -0,0 +1,67 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./Slider.css";
+
+// import required modules
+import { Pagination } from "swiper";
+
+//import images
+import Summer from '../image/Summer.png';
+import Winter from '../image/Winter.png';
+import Accessories from '../image/Accessories.png';
+import Sale from '../image/sale.png';
+import Logo from '../image/Logo.jpg';
+import { Link } from "react-router-dom";
+
+export default function Slider() {
+  return (
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="slider-text">
+            <h1>Welcome to Revelation!</h1>
+            <h4>Scroll down to shop</h4>
+          </div>
+          <img src={Logo} alt={"picture"} className={'images'}/>
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider-text">
+            <h1>Summer Collection</h1>
+            <p>Shop our summer range. We have all the gear you'll need to keep you cool, colourful and stylish all summer long.</p>
+            <b><Link to='/summer' className="shoplink">Shop Summer</Link></h5>
+          </div>
+          <img src={Summer} alt={"picture"} className={'images'}/>
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider-text">
+          <h1>Winter Collection</h1>
+            <p>Shop our winter range. We have all the gear you'll need to keep you warm.comfortable and stylish throughout the winter season.</p>
+            <h5><Link to='/winter' className="shoplink">Shop Winter</Link></h5>
+          </div>
+          <img src={Winter} alt={"picture"}/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider-text">
+          <h1>Accessories Collection</h1>
+            <p>Shop our accessories range, for the latest shades, bags, head gear and more.</p>
+            <h5><Link to='/accessories' className="shoplink">Shop Accessories</Link></h5>
+          </div>
+          <img src={Accessories} alt={"picture"} className={'images'} />
+        </SwiperSlide>
+        
+      </Swiper>
+    </>
+  );
+}
