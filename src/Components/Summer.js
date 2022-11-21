394c5013d25f4fbd3c69775963e28c529d6754e4
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-11-21 12:33:40 +0200

    order added

--- src/Components/Summer.js
+++ src/Components/Summer.js
@@ -10,8 +10,6 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import CircularProgress from '@mui/material/CircularProgress';
-
-
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -19,7 +17,6 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
-
 const Summer = (props) => {
     function GetUserUid() {
         const [uid, setUid] = useState(null);
@@ -90,16 +87,9 @@ const Summer = (props) => {
     };
     return (
         <>
-           
-
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {Array.from(Array(4)).map((_, index) => (
-
-    <Box sx={{ flexGrow: 1 }} marginLeft={4}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 20 }}>
-        {Array.from(Array(5)).map((_, index) => (
-
           <Grid item xs={2} sm={4} md={4} key={index}>
             {products.length > 0 && (
                 <div className='container-fluid'>
@@ -124,5 +114,4 @@ const Summer = (props) => {
         </>
     )
 }
-
 export default Summer;
